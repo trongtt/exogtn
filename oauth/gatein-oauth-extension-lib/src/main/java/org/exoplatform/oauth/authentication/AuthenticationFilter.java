335e0ee8105385ce114e6335bb0657ff3f816a8a
Minh Hoang TO <hoang281283@gmail.com>
2011-12-16 18:52:32 +0700

    Replace OAuthLoginServlet with AuthenticationFilter to remove OAuth relevant servlet from portal.war/WEB-INF/web.xml

--- oauth/gatein-oauth-extension-lib/src/main/java/org/exoplatform/oauth/authentication/AuthenticationFilter.java
+++ oauth/gatein-oauth-extension-lib/src/main/java/org/exoplatform/oauth/authentication/AuthenticationFilter.java
@@ -1,4 +1,4 @@
-/**
+/*
  * Copyright (C) 2011 eXo Platform SAS.
  *
  * This is free software; you can redistribute it and/or modify it
@@ -16,58 +16,66 @@
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
-package org.exoplatform.web.login;
+package org.exoplatform.oauth.authentication;
 
 import org.exoplatform.container.ExoContainerContext;
-import org.exoplatform.container.web.AbstractHttpServlet;
+import org.exoplatform.web.filter.Filter;
 import org.exoplatform.web.security.GateInToken;
 import org.exoplatform.web.security.security.TransientTokenService;
 import org.gatein.wci.security.Credentials;
-
 import java.io.IOException;
 import java.net.URI;
-
+import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * The Endpoint handles authentication
- * 
- * request must have parameters as callback, oauth_token
- * response will have parameters as oauth_token, oauth_login_token
- * 
- * If there is any error, response will be HttpServletResponse.SC_BAD_REQUEST
- * 
- * @author <a href="kienna@exoplatform.com">Kien Nguyen</a>
- * @version $Revision$
+ * A plugin of ExtensibleFilter service.
+ *
+ * It is visualized as a mandatory replacement of legacy OAuthLoginServlet class as
+ * we aim to deploy OAuth as GateIn extension
+ *
+ *TODO: Provide detailed documentation
+ *
+ * @author <a href="hoang281283@gmail.com">Minh Hoang TO</a>
+ * @date 12/16/11
  */
-
-@SuppressWarnings("serial")
-public class OAuthLoginServlet extends AbstractHttpServlet
+public class AuthenticationFilter implements Filter
 {
    private static final String OAUTH_TOKEN = "oauth_token";
+
    private static final String OAUTH_LOGIN_TOKEN = "oauth_login_token";
+
    private static final String AUTH_TOKEN = "auth_token";
 
-   protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+   public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException
    {
-      TransientTokenService tokenService =
-         (TransientTokenService)ExoContainerContext.getCurrentContainer().getComponentInstanceOfType(
-            TransientTokenService.class);
+      process((HttpServletRequest)request, (HttpServletResponse)response);
+   }
+
+   private void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+   {
+      TransientTokenService tkService = (TransientTokenService)ExoContainerContext.getCurrentContainer().getComponentInstanceOfType(TransientTokenService.class);
+
       String authToken = request.getParameter(AUTH_TOKEN);
-      if (authToken != null) //After login successfully
+
+      //TODO: Explain this check
+      if (authToken != null)
       {
-         if (request.getRemoteUser() != null && tokenService.getToken(authToken) != null)
+         if (request.getRemoteUser() != null && tkService.getToken(authToken) != null)
          {
-            GateInToken oToken = tokenService.getToken(authToken);
+            GateInToken oToken = tkService.getToken(authToken);
             String callbackURL = oToken.getPayload().getUsername();
             String oauthToken = oToken.getPayload().getPassword();
-            
+
             //Create another transient token to send consumer it
-            String loginToken = tokenService.createToken(new Credentials(request.getRemoteUser(), oauthToken));
-            tokenService.deleteToken(authToken);
-            
+            String loginToken = tkService.createToken(new Credentials(request.getRemoteUser(), oauthToken));
+            tkService.deleteToken(authToken);
+
+            //TODO: Examine security leak as password is injected into redirect response
             callbackURL += "?" + OAUTH_LOGIN_TOKEN + "=" + loginToken + "&" + OAUTH_TOKEN + "=" + oToken.getPayload().getPassword();
             response.sendRedirect(response.encodeRedirectURL(callbackURL));
          }
@@ -92,9 +100,9 @@ public class OAuthLoginServlet extends AbstractHttpServlet
                callbackURL =
                   request.getLocalAddr() + (callbackURL.startsWith("/") ? uri.toString() : "/" + uri.toString());
             }
-            
+
             //Create a transient token with Credentials: callbackURL(as username) and oauth_token (as password)
-            authToken = tokenService.createToken(new Credentials(callbackURL, token));
+            authToken = tkService.createToken(new Credentials(callbackURL, token));
             String initialURI = request.getRequestURI() + "?" + AUTH_TOKEN + "=" + authToken;
             String redirectURI = request.getContextPath() + "/dologin?initialURI=" + initialURI;
             response.sendRedirect(response.encodeRedirectURL(redirectURI));
@@ -104,10 +112,5 @@ public class OAuthLoginServlet extends AbstractHttpServlet
             response.sendError(HttpServletResponse.SC_BAD_REQUEST, e.getMessage());
          }
       }
-   }
-
-   protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
-   {
-      doGet(request, response);
    }
 }
