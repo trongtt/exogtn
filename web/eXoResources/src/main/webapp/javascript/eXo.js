cc4a5359750f72267fdad05fc477babae75b2a22
phuong_vu <phuong.vu@exoplatform.com>
2012-03-05 17:42:39 +0700

    Move generateId method from DOMUtil to eXo.js

--- web/eXoResources/src/main/webapp/javascript/eXo.js
+++ web/eXoResources/src/main/webapp/javascript/eXo.js
@@ -147,6 +147,13 @@ eXo.session.destroyItv = function () {
    session.itvObj = null ;
 } ;
 
+/**
+ * Generates an id based on the current time and random number
+ */
+eXo.generateId = function(objectId) {
+	return (objectId + "-" + new Date().getTime() + Math.random().toString().substring(2)) ;
+};
+
 eXo.debug = function(message) {
 	if(!eXo.developing) return;
 	
